7c0bb23e979827523d5eee166f2d02a0c0e16db5
Anuj Maurice <anuj.maurice@gmail.com>
2011-09-04 14:04:26 +0530

    some utilities

---
+++ utilities/prettycontact.py
@@ -0,0 +1,21 @@
+import os
+import time
+path_to_contact = '/home/anuj/backup/contacts_1_9_2011/'
+files = os.listdir(path_to_contact)
+files.sort()
+excpt = []
+out ="%s \n"%(time.ctime())
+for file in files:
+	if file.split('.')[-1] =='vcf':
+		try:
+			lines = open(path_to_contact+file).readlines()
+			out += "%s  : %s\n"%(lines[2].split(';')[3].replace('=20',' '),lines[3].split(';')[-1].split(':')[1].strip())
+		except:
+			excpt.append(file)
+
+if excpt:
+	print excpt
+
+output = open('contact.txt','w')
+output.write(out)
+output.close()
